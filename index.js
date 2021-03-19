2141b46d57d27f21312b6f01f286bcc6793a9f9b
Tomas Storc <tomas.storc@gmail.com>
2021-03-19 18:59:47 +0100

    added extended false

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(cors())
-app.use(express.urlencoded());
+app.use(express.urlencoded({extended: false}));
 require('dotenv').config();
 const mongoose = require('mongoose');
 const dbUrl = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eznnv.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
